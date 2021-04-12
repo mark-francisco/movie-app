6972e2355026b8ed876fc981cd25a48e163605a8
Mark Francisco <mfrancisco581@gmail.com>
2021-04-12 02:15:00 -0400

    Update production.rb file config.assets.compile to true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "example.com", "127.0.0.1:5500"
+    origins "localhost:8080", "example.com", "127.0.0.1:5500", "sleepy-waters-75776.herokuapp.com/"
 
     resource "*",
       headers: :any,
